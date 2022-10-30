84c4e1c2d45e0c56d4acb216d32379eb5501f1d4
Vitali Malykh <makingmeth@gmail.com>
2022-10-30 21:16:09 +0300

    feat: labal avatar

--- src/app/GameScreen/Seat/Label.ts
+++ src/app/GameScreen/Seat/Label.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-cycle */
 import { Assets } from "@pixi/assets";
-import { Container, Sprite, Text } from "pixi.js";
+import { Container, Graphics, Sprite, Text } from "pixi.js";
 import { app } from "../../../app";
 import { PlayerState } from "../../../lib/states/PlayerState";
 
@@ -9,26 +9,38 @@ export default class Label extends Container {
     background: Sprite;
 
     playerState: PlayerState;
-
+    
     avatar: Sprite;
-
+    
     nameText: Text;
-
+    
     bankText: Text;
-
+    
     constructor(playerState: PlayerState) {
         
         super()
-
+        
         this.background = new Sprite(Assets.cache.get('avatar_player_active_1'));
         this.addChild(this.background)
-
+        
         this.playerState = playerState;
-
-        this.avatar = playerState.avatar;
+        
+        this.avatar = new Sprite(playerState.avatar.texture);
+        this.avatar.anchor.set(0.5, 0.5)
+        this.avatar.setTransform(
+            this.avatar.height/2 -21,
+            this.height/2 - 3
+        )
         this.avatar.scale.set(this.background.height/playerState.avatar.height)
-        this.addChild(this.avatar)
-
+        this.addChild(this.avatar);
+        
+        const avatarMask = new Graphics();
+        avatarMask.beginFill(0xffffff, 1);
+        avatarMask.drawCircle(0, 0, this.avatar.width/2 + 5);
+        avatarMask.endFill();
+        this.avatar.addChild(avatarMask);
+        this.avatar.mask = avatarMask;
+        
         this.nameText = new Text (`${app.playerState!.PlayerName}`, {
             fontFamily: 'Bebas Neue',
             fontWeight: '600',
@@ -36,10 +48,10 @@ export default class Label extends Container {
             fill: 0xFDDA24,
         });
         this.nameText.anchor.set(0.5, 0.5)
-        this.nameText.x = this.width/3 * 2;
+        this.nameText.x = this.width/3 * 1.8;
         this.nameText.y = this.height/10 * 3;
         this.addChild(this.nameText);
-
+        
         this.bankText = new Text (`$ ${app.playerState!.bank}`, {
             fontFamily: 'Bebas Neue',
             fontWeight: '600',
