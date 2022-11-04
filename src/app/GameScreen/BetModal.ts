8e4781c955ab9ff11a69ebebe4865dcd4fca484a
Vitali Malykh <makingmeth@gmail.com>
2022-11-04 05:26:33 +0300

    feat: bet modal chip rotation offset

--- src/app/GameScreen/BetModal.ts
+++ src/app/GameScreen/BetModal.ts
@@ -46,7 +46,7 @@ export default class BetModal extends Container {
     getStack(val: ChipVal, amount: number, index: number): void {
         this[`stack${val}`] = new Container();
         // eslint-disable-next-line no-nested-ternary
-        this[`stack${val}`]!.x = (index < 3) ? 200 + index * 250 : (index < 6 ) ? 200 + ((index-3) * 250) : 200 + ((index-6) * 250);
+        this[`stack${val}`]!.x = (index < 3) ? 250 + index * 250 : (index < 6 ) ? 250 + ((index-3) * 250) : 250 + ((index-6) * 250);
         // eslint-disable-next-line no-nested-ternary
         this[`stack${val}`]!.y = (index < 3) ? 200 : (index < 6 ) ? 400 : 600
         this[`stack${val}`]!.scale.set(0.8)
@@ -56,6 +56,8 @@ export default class BetModal extends Container {
         for (let i = 0; i < amount; i++) {
             const chip = new Sprite(Assets.cache.get(`${val}`));
             chip.y = 0 - 15*i;
+            chip.anchor.set(0.5);
+            chip.rotation = (i === amount - 1) ? 0 : Math.random();
             this[`stack${val}`]?.addChild(chip);
         }
     }
